51573d863f9fd894bdfa739281b129b294fc3998
Kylia Miskell <8714037+kmiskell@users.noreply.github.com>
2026-09-21 16:35:44 -0700

    Add cors to test frontend

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('data', (req, res) => {
     return JSON.stringify(req.body)
